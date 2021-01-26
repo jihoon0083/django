fe39811ece8f04a2a3df0ea395741df5aa35a023
jihoon kim <jihoon0083@gmail.com>
2021-01-26 14:38:57 +0900

    no message

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include , path
 
-urlpatterns = [
+urlpatterns =  [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
